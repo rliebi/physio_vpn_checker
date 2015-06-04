957a3d6173b43e7fe212cadaa4fed557065b182f
Remo Liebi <rliebi@hsr.ch>
2015-06-04 20:25:17 +0200

    renamed package

--- checker/settings.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'remoliebi'
-
-CHECK_INTERVAL = 10  # in seconds
-PAUSE_WHEN_RESTARTING_L2TP = 30  # in seconds
-SEND_NOTIFICATION_AFTER = 2  # in retries
-SEND_NOTIFICATION_EVERY = 10  # in retries
-NOTIFY_LOCATION_AFTER = 10  # in retries
-RESTART_L2TP_AFTER = 30  # if a single location fails, i will restart the l2tp server after xx retries
-PROWL_NOTIFY_API_KEYS = ['d14faac0d0225b02bdf268926d214cd12dd6a2bb', ]
