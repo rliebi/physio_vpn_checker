f751ab567b45accfdff7c85109f3a7f21fa742d9
Remo Liebi <rliebi@hsr.ch>
2015-06-04 20:25:40 +0200

    renamed package

---
+++ measurement.py
@@ -0,0 +1,18 @@
+import subprocess
+import time
+from physio_vpn_checker import settings, pusher
+
+__author__ = 'remoliebi'
+
+
+def restart_ipsec():
+    command = ['/usr/sbin/service', 'ipsec', 'restart']
+    # shell=FALSE for sudo to work.
+    subprocess.call(command, shell=False)
+    time.sleep(settings.PAUSE_WHEN_RESTARTING_L2TP)
+
+
+def ipsec_status():
+    p = subprocess.Popen(['program', 'arg1'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output, err = p.communicate(b"input data that is passed to subprocess' stdin")
+    pusher.push('IPSec Status', output)
