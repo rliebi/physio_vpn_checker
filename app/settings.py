3292393106630c3f151b3abb8caf24eb3f64dd87
Remo Liebi <rliebi@hsr.ch>
2015-06-10 17:31:16 +0200

    removed api keys from log

--- app/settings.py
+++ app/settings.py
@@ -17,11 +17,10 @@ CHECK_INTERVAL = 10  # in seconds
 PAUSE_WHEN_RESTARTING_L2TP = 20  # in seconds
 SEND_NOTIFICATION_AFTER = 5  # in retries
 SEND_NOTIFICATION_EVERY = 10  # in retries
-NOTIFY_LOCATION_AFTER = 30  # in retries
+NOTIFY_LOCATION_AFTER = 12  # in retries
 PROWL_APP_NAME = os.environ.get('app_name', 'PHYSIOZENTRUM')
 RESTART_L2TP_AFTER = 30  # if a single location fails, i will restart the l2tp server after xx retries
 PROWL_NOTIFY_API_KEYS = [value for value in get_multiple_env_args('PROWL_API_KEY')]
-print 'API KEYS', PROWL_NOTIFY_API_KEYS
 SMTP_PASS = os.environ.get('SMTP_PASS')
 SMTP_HOST = 'plesk.liebi.net'
 SMTP_USER = 'noreply@lnh.ch'
